8abdf3a6feafc118bcfe6c8fd753fa8aadae733b
TychoBrouwer <tychobrouwer33@gmail.com>
2023-02-27 16:40:41 +0100

    server update srcipts

---
+++ update-server.bat
@@ -0,0 +1,2 @@
+@echo off
+ssh root@192.168.178.106 "cd /var/www/html/personal-website-elm && git pull origin main"
---
+++ update-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh root@192.168.178.106 "cd /var/www/html/personal-website-elm && git pull origin main"
