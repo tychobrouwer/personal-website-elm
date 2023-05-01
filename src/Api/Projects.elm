63d470925164c384cb3f33ce6e8bf62c68149b62
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-05-01 11:42:06 +0200

    prod fix

--- src/Api/Projects.elm
+++ src/Api/Projects.elm
@@ -21,7 +21,7 @@ get :
     -> Cmd msg
 get options =
     Api.Token.get
-        { url = "http://www.tbrouwer.com/api/projects.json"
+        { url = "https://www.tbrouwer.com/api/projects.json"
         , expect =
             Api.Data.expectJson options.onResponse
                 (Json.field "projects" (Json.list projectDecoder))
